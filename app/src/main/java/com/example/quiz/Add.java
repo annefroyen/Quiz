b907d1ffed6b2c24a7d31bfc2699622f2829165c
Anne H F <annehjefro@gmail.com>
2019-01-28 23:00:01 +0100

    this is a quizz

--- app/src/main/java/com/example/quiz/Add.java
+++ app/src/main/java/com/example/quiz/Add.java
@@ -21,6 +21,7 @@ public class Add extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
